95e5866ea5b1dfa6e98854a7b4721fe621062573
LostBeard <LostBeard@users.noreply.github.com>
2026-08-07 17:12:00 +0000

    Deploying to gh-pages from @ LostBeard/SpawnDev.SpawnJS.RazorRenderer@110890f63e678e1afffcafd817fcb7c0b8482de8 🚀

---
+++ content.js
@@ -0,0 +1,21 @@
+﻿// Script loader works in window and worker scopes
+(async function () {
+    async function loadScript(src) {
+        src = chrome.runtime.getURL(src);
+        if (typeof globalThis.importScripts === 'function') {
+            globalThis.importScripts(src);
+        } else if (globalThis.document) {
+            const script = document.createElement('script');
+            const loadTask = new Promise((onload, onerror) => Object.assign(script, { onload, onerror, src }));
+            (document.head || docuemnt.documentElement).append(script);
+            await loadTask;
+        }
+    }
+    // Load anything that needs to load before .Net Wasm
+    //
+    // Synchronously fired events need to be captured by Javascript and
+    // held for .Net Wasm to pick up and handle once it loads.
+    // 
+    // Load .Net Wasm app
+    await loadScript('app/main.classic.js');
+})();
