e6029f40d1934519fdd9125d97c07d2ae3b0fd15
Todd Tanner <todd@spawndev.com>
2026-08-07 13:01:44 -0400

    Added implicit RazorRenderer service registration when using RootComponents

--- SpawnDev.SpawnJS.RazorRenderer/IServiceCollectionExtensions.cs
+++ SpawnDev.SpawnJS.RazorRenderer/IServiceCollectionExtensions.cs
@@ -13,16 +13,23 @@ namespace SpawnDev.SpawnJS.RazorRenderer
         /// .NET WASM and applies render batches directly to the DOM via SpawnJS. One renderer hosts many
         /// root components; mount them with <see cref="SpawnDomRenderer"/>'s <c>RenderComponentAsync</c>.
         /// </summary>
-        public static IServiceCollection AddRazorRenderer(this IServiceCollection _this)
+        public static IServiceCollection AddRazorRenderer(this IServiceCollection _this) => _this.AddRazorRenderer(out var _);
+        /// <summary>
+        /// Adds the <see cref="SpawnDomRenderer"/> singleton - an interactive Blazor renderer that runs in
+        /// .NET WASM and applies render batches directly to the DOM via SpawnJS. One renderer hosts many
+        /// root components; mount them with <see cref="SpawnDomRenderer"/>'s <c>RenderComponentAsync</c>.
+        /// </summary>
+        public static IServiceCollection AddRazorRenderer(this IServiceCollection _this, out SpawnJSRootComponentMappingCollection rootComponentMappings)
         {
             _this.TryAddSingleton<SpawnDomRenderer>();
             var existing = _this.FirstOrDefault(o => o.ServiceType == typeof(SpawnJSRootComponentMappingCollection));
             var ret = existing?.ImplementationInstance as SpawnJSRootComponentMappingCollection;
             if (ret == null)
             {
-                var newRet = new SpawnJSRootComponentMappingCollection();
-                _this.AddSingleton<SpawnJSRootComponentMappingCollection>(newRet);
+                ret = new SpawnJSRootComponentMappingCollection();
+                _this.AddSingleton<SpawnJSRootComponentMappingCollection>(ret);
             }
+            rootComponentMappings = ret;
             return _this;
         }
     }
--- SpawnDev.SpawnJS.RazorRenderer/SpawnJSAppBuilderExtensions.cs
+++ SpawnDev.SpawnJS.RazorRenderer/SpawnJSAppBuilderExtensions.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-
-namespace SpawnDev.SpawnJS.RazorRenderer
+﻿namespace SpawnDev.SpawnJS.RazorRenderer
 {
     /// <summary>
     /// Adds extenions methods to SpawnJSAppbuilder
@@ -10,18 +8,15 @@ namespace SpawnDev.SpawnJS.RazorRenderer
         extension(SpawnJSAppBuilder builder)
         {
             /// <summary>
-            /// Root components that RazorRenderer will render when started in a browser Window global scope
+            /// Root components that RazorRenderer will render when started in a browser Window global scope.<br/>
+            /// RazorRenderer service is registered if not already registered.
             /// </summary>
             public SpawnJSRootComponentMappingCollection RootComponents
             {
                 get
-                {
-                    var existing = builder.Services.FirstOrDefault(o => o.ServiceType == typeof(SpawnJSRootComponentMappingCollection));
-                    var ret = existing?.ImplementationInstance as SpawnJSRootComponentMappingCollection;
-                    if (ret != null) return ret;
-                    var newRet = new SpawnJSRootComponentMappingCollection();
-                    builder.Services.AddSingleton<SpawnJSRootComponentMappingCollection>(newRet);
-                    return newRet;
+                {   
+                    builder.Services.AddRazorRenderer(out var rootComponentMappings);
+                    return rootComponentMappings;
                 }
             }
         }
